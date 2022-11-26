8cbba9813395c8f30de493e449dbc41d1935b8d5
Akhil Reddy <avr54@scarletmail.rutgers.edu>
2022-11-26 18:58:46 -0500

    put comments for groupanagrams

--- Arrays & Hashing/GroupAnagrams.py
+++ Arrays & Hashing/GroupAnagrams.py
@@ -3,6 +3,23 @@ from typing import List
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         
+
         
         
         return True
+
+
+
+# Logic:
+
+# you are going to create a hashmap, keys are going to be "counters" eachw word
+# and the values would be the words themselves
+
+# hence, for a single key, all the values would be the anagrams
+
+# Pseudocode: 
+
+# loop through the list once, for each word in the list
+# create an array to hold the counter (size of 26)
+# now, for values with that counter in the array, add value to the key
+# once for loop finishes, we are good
