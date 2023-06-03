e5fece5f794dc99bc2717b87a2d61577d92b9605
Akhil Reddy <avr54@scarletmail.rutgers.edu>
2023-06-03 18:12:22 -0400

    Added ContainsDuplicate

---
+++ Arrays & Hashing/ContainsDuplicate.py
@@ -0,0 +1,11 @@
+from ast import List
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
+        for i in nums:
+            if i not in hashset:
+                hashset.add(i)
+            else:
+                return True
+        return False
